5fb1cf3884abd15b448d7fe0dead2619a5dfd9d7
Durgeshwari_codes <sheryasahu505@gmail.com>
2026-04-05 13:44:25 +0530

    Add files via upload

---
+++ 03-javascript/06_forOfLoop/01_forOF.js
@@ -0,0 +1,13 @@
+let library=["80,81,82,83,84,84,86,87,88,89,90"];
+for(seats of library){
+    console.log(seats);
+}
+// nested loops in for of loop
+
+let numbers=[[1,2,3],[4,5,6],[7,8,9,]];
+for(num of numbers){
+    console.log(num);
+    for(n of num){
+        console.log(n);
+    }
+}
