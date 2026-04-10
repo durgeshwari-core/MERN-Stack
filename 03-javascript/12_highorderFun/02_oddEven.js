fdc170fcca346009cb9aef5b6f2eb29e22dab9cf
Durgeshwari_codes <sheryasahu505@gmail.com>
2026-04-10 10:17:33 +0530

    Add files via upload

---
+++ 03-javascript/12_highorderFun/02_oddEven.js
@@ -0,0 +1,21 @@
+function oddEvenFactory(request) {
+    if (request == "odd") {
+        let odd = function (n) {
+            console.log(!(n % 2 == 0));
+        }
+        return odd;
+    }
+     else if (request == "even") {
+        let even = function (n) {
+            console.log(n % 2 == 0);
+        }
+        return even;
+    }
+    else{
+        console.log("wrong request");
+    }
+}
+let request="odd"//or even
+
+let func=oddEvenFactory(request);
+
