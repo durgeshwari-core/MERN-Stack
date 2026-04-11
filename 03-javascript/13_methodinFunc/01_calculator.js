7510c44b572a81e9a43357d2d0f6423cd077f696
Durgeshwari_codes <sheryasahu505@gmail.com>
2026-04-11 18:07:02 +0530

    Add files via upload

---
+++ 03-javascript/13_methodinFunc/01_calculator.js
@@ -0,0 +1,13 @@
+// using objects
+const calculator={
+    num:23,
+    add:function(a,b){
+          return a+b;
+    },
+    sub:function(a,b){
+          return a-b;
+    },
+    mult:function(a,b){
+          return a*b;
+    }
+};
