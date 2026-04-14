34fc4b3831f1518f72e2ff777010784c2d81253b
Durgeshwari_codes <sheryasahu505@gmail.com>
2026-04-14 12:13:54 +0530

    Add files via upload

---
+++ 03-javascript/27_oop.js
@@ -0,0 +1,8 @@
+function createBiscuits(nam,price,qlt,cy,com){
+    this.name=nam;
+    this.price=price;
+    this.qlt=qlt;
+    this.category=cy;
+    this.company=com;
+}
+let biscuit1= new createBiscuits("parleG",10,10,"regular","orea")
