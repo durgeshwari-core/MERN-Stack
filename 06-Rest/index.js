de0521313842fb2b96f768f0c3a5a5e4e40c2bcf
Durgeshwari sahu <sheryasahu505@gmail.com>
2026-04-16 16:16:28 +0530

    change

---
+++ 06-Rest/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+const path = require("path");
+
+// to let apis understand all request
+app.use(express.urlencoded({ extended: true }));
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.set(express.static(path.join(__dirname, "public")));
+
+let post = {
+    username: "Shreya",
+    title: "My First Post",
+},
+{
+    username: "Durgeshwari",
+    title: "another Post",
+},
+{
+    username: "apsara",
+    title: "unltra pro",
+}
+
+app.get("/", (req, res) => {
+    res.send("Welcome to the REST API");
+});
+
+app.listen(port, () => {
+  console.log("Server is listening on port :8080");
+});
