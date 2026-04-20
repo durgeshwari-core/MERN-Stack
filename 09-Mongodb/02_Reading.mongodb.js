8eec5a03852fd035f2798c71bb38236d30a1e133
Durgeshwari sahu <sheryasahu505@gmail.com>
2026-04-20 14:53:35 +0530

    save changes

--- 09-Mongodb/02_Reading.mongodb.js
+++ 09-Mongodb/02_Reading.mongodb.js
@@ -1,10 +1,16 @@
 use('Ecommerce')
 // db.products.find({categor: 'Electronics'})
 
-db.products.find().pretty()
+// db.products.find().pretty()
 
-db.products.find({ price: { $gt: 1000 } }) // greater than 1000
-db.products.find({ price: { $gte: 1000, $lte: 50000 } })
+// db.products.find({ price: { $gt: 1000 } }) // greater than 1000
+// db.products.find({ price: { $gte: 1000, $lte: 50000 } })
 
-db.products.find({}, { name: 1, price: 1, _id: 0 })
+// db.products.find({}, { name: 1, price: 1, _id: 0 })
 
+// projection-> specific data
+// db.products.find({}, { name: 1, price: 1, _id: 0 })
+
+// sorting and limiting -> for pagenation
+
+db.products.find().sort({ price: -1 }).limit(1)
