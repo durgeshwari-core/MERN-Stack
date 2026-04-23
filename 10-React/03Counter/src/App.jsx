9f905847f981a523fd83e32c69d5fa2f30b64d31
Durgeshwari sahu <sheryasahu505@gmail.com>
2026-04-23 23:59:02 +0530

    ..

--- 10-React/03Counter/src/App.jsx
+++ 10-React/03Counter/src/App.jsx
@@ -8,12 +8,16 @@ function App() {
   const [count, setCount] = useState(0)
   let counter =5;
 
+  const addValue = () =>{
+    console.log("add avlue ",Math.random());
+  }
+
   return (
     <>
     <h1>shreya</h1>
     <h3> hey!! how are you?</h3>
     <h5>counter:{counter}</h5>
-    <button> ADD</button>
+    <button onClick={addValue}> ADD</button>
     <br />
     <button> REMOVE</button>
 
