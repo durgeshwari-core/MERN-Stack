3e6306500da4bb954ca13eb133d0df1f3b9dcf9e
Durgeshwari sahu <sheryasahu505@gmail.com>
2026-04-26 15:09:18 +0530

    components

---
+++ 10-React/05components/vite-project/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+    <div id = "father">
+      <h1 id = "child1">My React App</h1>
+      <h2 id ="child2">Welcome to My App</h2>
+    </div>
+    <div id="mother"> </div>
+    </>
+
+  );
+};
+
+export default App;
